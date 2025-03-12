a2d732d2382c3d36c3854908702f879efb1e48c5
Mathew Caldwell <20143330@tafe.wa.edu.au>
2025-03-12 15:04:16 +0800

    added test_multiply() function

--- test_functions.py
+++ test_functions.py
@@ -1,6 +1,5 @@
 from functions import add, subtract, multiply
 from functions import convert_fahrenheit_to_celsius as f2c
-import pytest
 
 def test_add():
     assert add(2, 3) == 5
@@ -9,6 +8,9 @@ def test_add():
 # uncomment the following test in step 5
 def test_subtract():
     assert subtract(2, 3) == -1
+
+def test_multiply():
+    assert multiply(2,5) == 10
 
 # uncomment the following test in step 11
 # def test_convert_fahrenheit_to_celsius():
